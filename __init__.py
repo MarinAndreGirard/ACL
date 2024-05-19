96336a2573b35f5d31a26369c17af9fcfa082748
MarinAndreGirard <marin.girard@outlook.com>
2024-05-19 15:37:56 +0200

    Making the Kernell/Libraries/Jupyter work.

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@
 # my_quantum_simulator/__init__.py
 
 # Import key modules and classes to make them available when the entire package is imported
-from .core import QuantumSystem, DecoherenceModel
-from .simulation import SimulationEngine
-from .utils import helpers
+from .core import create_hamiltonian, creat_state, time_evo
+#from .simulation import SimulationEngine
+#from .utils import helpers
