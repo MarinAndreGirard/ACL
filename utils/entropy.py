b94a20be8d72ff0bb0200bd81bc0184464533c2b
MarinAndreGirard <marin.girard@outlook.com>
2024-05-19 22:00:09 +0200

    Running some entropy based tests showing that the physics is sound

--- utils/entropy.py
+++ utils/entropy.py
@@ -3,6 +3,8 @@ import qutip as qt
 import math
 import matplotlib.pyplot as plt
 
+#TODO find a way to get ride of the runntime warning that comes everytime I run this
+
 def compute_VN(result, time_index, subsystem_index=0):
     density_matrix = qt.ptrace(result.states[time_index], [subsystem_index])  # Calculate the density matrix at the specified time
     entropy = -np.sum(np.nan_to_num(np.log2(np.linalg.eigvals(density_matrix.full())) * np.linalg.eigvals(density_matrix.full())))
