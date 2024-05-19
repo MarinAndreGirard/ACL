b8c5d1cbe6b1dd635c49186e877b027f2bc3fdd8
MarinAndreGirard <marin.girard@outlook.com>
2024-05-19 12:26:50 +0200

    Added create_state and create_hamiltonian core functions

---
+++ core/create_hamiltonian.py
@@ -0,0 +1,47 @@
+import numpy as np
+import qutip as qt
+
+#H takes 5 parameters, + 2 dimensions + option selecting
+#E_spacing for qs, E_int interaction factor, E_int2 constant added to env part of interaction term
+# E_env env self interaction factor, E_env2 constant part of env self interaction term
+def create_H(d1 = 2,d2 = 200, E_spacing = 1.0, E_int = 0.03, E_int2=0, E_env=1, E_env2=0):
+    """Creates Hamiltonian for simulation
+
+    Args:
+        d1 (int, optional): dimension of system. Defaults to 2.
+        d2 (int, optional): dimension of environment. Defaults to 200.
+        E_spacing (float, optional): SHO parameter. Defaults to 1.0.
+        E_int (float, optional): interaction strength between the environment and system. Defaults to 0.03.
+        E_int2 (int, optional): constant energy factor of interaction. Defaults to 0.
+        E_env (int, optional): environment self interaction energy factor. Defaults to 1.
+        E_env2 (int, optional): constant energy factor of enviroment self interaction. Defaults to 0.
+
+    Returns:
+        int, Qobj,Qobj,Qobj...: _description_...TODO
+    """
+    d = d1*d2  # Total Hilbert space dimension
+
+    # System self interaction. Is the self interaction of a truncated simple harmonic oscillator
+    # We also want the option to make it a random matrix or the identity.
+    # or an identity matrix with simple spacing (which might just be my SHO anyways) TODO
+    
+    H_s_self = qt.qeye(d1) #TODO add other options/for self interaction of system, use E_Spacing?
+    H_s = qt.tensor(H_s_self, qt.qeye(d2)) # Makes the full system Hamiltonian
+    
+    # System-environment interaction. Is the interaction of a truncated simple harmonic oscillator with a bath
+    
+    diagonal_elements = np.arange(0, d1) * E_spacing   
+    H_int_s = qt.Qobj(np.diag(diagonal_elements)) # Creat the SHO part of the interaction Hamiltonian TODO is that what it is supposed to be?
+    H_int_e = E_int *qt.rand_herm(d2,1) + E_int2 * qt.qeye(d2)
+    H_int = qt.tensor(H_int_s, H_int_e) # Makes the full interaction Hamiltonian
+    
+    # Environment self interaction. Is the self interaction of a bath
+    H_e_self = E_env * qt.rand_herm(d2,1) + E_env2 * qt.qeye(d2)  # Random Hermitian matrix for system 2
+    H_e = qt.tensor(qt.qeye(d1), H_e_self) # Makes the full environment Hamiltonian
+    
+    # Define the total Hamiltonian
+    H_total = H_s + H_int + H_e
+    
+    #TODO why is only H_int_s Qobj
+
+    return d, H_total, H_s, H_int, H_e, H_s_self, H_int_s, H_int_e, H_e_self
---
+++ core/create_state.py
@@ -0,0 +1,34 @@
+import numpy as np
+import qutip as qt
+import math
+
+def create_H(d1,d2, H_e_self,w=[0]):
+    
+    #We give the choice of how which state superposition to use. and how many (not more than d1)
+    #we give the choice of what enviroment state, ie can choose a random state, a env self int eigenstate or interaction eig, or total Hamiltonian  env eig
+    #Do we superimpose pointer states? They depend on the system self interaction? 
+    #Do we add the option of not starting in a product state? TODO DO ALL OF THAT
+
+    #d1 = 10,d2 = 200,w = 0.3, E_spacing = 1.0, Int_strength = 0.03, tmax= 10, ind_nb = 100,log=0,k0=2, k1=7
+    #d1 = 10,d2 = 200,w = 0.3, E_spacing = 1.0, Int_strength = 0.03, tmax= 10, ind_nb = 100,log=0,envi=[1,2]
+    #TODO use these things above to make the function more general. ie, go see the other generate_result in the q_solve.py.
+
+    ket_0 = qt.basis(d1, round(d1/4))  # |0> state
+    ket_1 = qt.basis(d2, round(d1*3/4))  # |2> state, int(dim_system_1/2)
+
+    # Create basis states for system 1 and system 2
+    #basis_system_1 = [qt.basis(d1, i) for i in range(d1)]
+    #basis_system_2 = [qt.basis(d2, i) for i in range(d2)]
+    
+    if w ==[0]:
+        w_ = 0.3
+        state_s = (math.sqrt(w_)*ket_0 + math.sqrt(1-w_)*ket_1).unit()
+
+    #initial_state_system_2 = qt.rand_ket(dim_system_2)
+    ev ,es = H_e_self.eigenstates()
+    state_e = es[round(d2/2)]#TODO is there a better way to do this
+    #define initial state of full system
+    state = qt.tensor(initial_state_system_1, initial_state_system_2)
+
+    return state
+    
